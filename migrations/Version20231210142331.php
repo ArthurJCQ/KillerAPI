4527fb92d8176aa9fa7df7126f4b2c35acb91c97
Arthur <arthurjcq@protonmail.com>
2023-12-10 15:59:42 +0100

    Fix/clean bumped code (#129)
    * Clean user password, fix room ID
    
    * fixup! Clean user password, fix room ID

---
+++ migrations/Version20231210142331.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20231210142331 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP SEQUENCE room_id_seq CASCADE');
+        $this->addSql('ALTER TABLE player DROP password');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('CREATE SEQUENCE room_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('ALTER TABLE player ADD password VARCHAR(255) NOT NULL');
+    }
+}
