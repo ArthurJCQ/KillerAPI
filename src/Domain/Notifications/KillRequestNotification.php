598f83c3bc09188bd0d7ea977c3c150587e491d4
miskoune <miskoune@proton.me>
2025-10-16 22:10:17 +0200

    feat: update notifications content (#174)

--- src/Domain/Notifications/KillRequestNotification.php
+++ src/Domain/Notifications/KillRequestNotification.php
@@ -6,6 +6,6 @@ namespace App\Domain\Notifications;
 
 final class KillRequestNotification extends KillerNotification
 {
-    public string $title = 'Tu as été tué !';
-    public string $content = 'Confirme ta mort dès maintenant...';
+    public string $title = 'Votre vie est en jeu';
+    public string $content = "Quelqu'un affirme avoir exécuté votre mission. Le coup fatal a-t-il été porté ? Répondez vite pour vérifier le meurtre !";
 }
