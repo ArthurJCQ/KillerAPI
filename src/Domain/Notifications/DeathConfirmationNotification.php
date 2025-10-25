a92e10bb6e6c0135b551c70909fa6007d0d1b366
miskoune <miskoune@proton.me>
2025-10-25 10:27:47 +0200

    refactor: improve notification content formatting for clarity

--- src/Domain/Notifications/DeathConfirmationNotification.php
+++ src/Domain/Notifications/DeathConfirmationNotification.php
@@ -7,5 +7,6 @@ namespace App\Domain\Notifications;
 final class DeathConfirmationNotification extends KillerNotification
 {
     public string $title = 'Mission accomplie';
-    public string $content = 'Votre cible a confirmé sa mort. Votre mission est réussie mais ce n\'est pas la fin de la chasse !';
+    public string $content = 'Votre cible a confirmé sa mort. ' .
+        'Votre mission est réussie mais ce n\'est pas la fin de la chasse ! ';
 }
--- src/Domain/Notifications/KillRequestNotification.php
+++ src/Domain/Notifications/KillRequestNotification.php
@@ -7,5 +7,6 @@ namespace App\Domain\Notifications;
 final class KillRequestNotification extends KillerNotification
 {
     public string $title = 'Votre vie est en jeu';
-    public string $content = "Quelqu'un affirme avoir exécuté votre mission. Le coup fatal a-t-il été porté ? Répondez vite pour vérifier le meurtre !";
+    public string $content = 'Quelqu\'un affirme avoir exécuté votre mission. Le coup fatal a-t-il été porté ?' .
+        'Répondez vite pour vérifier le meurtre !';
 }
